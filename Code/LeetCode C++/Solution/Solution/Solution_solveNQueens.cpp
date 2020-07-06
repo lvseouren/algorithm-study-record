b0ab25efd5067c45ccb9e5cb1ff3e9cd2362e93e
linge <602633512@qq.com>
2020-07-06 12:03:55 +0800

    https://leetcode-cn.com/problems/n-queens/ n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
    //回溯法，自己写了一边

--- Code/LeetCode C++/Solution/Solution/Solution_solveNQueens.cpp
+++ Code/LeetCode C++/Solution/Solution/Solution_solveNQueens.cpp
@@ -6,9 +6,10 @@ Solution_solveNQueens::Solution_solveNQueens()
 
 void Solution_solveNQueens::RunTest()
 {
-	int n = 4;
+	int n = 3;
 	solveNQueens(n);
 	int index = 0;
+	cout << n << "皇后问题共有解法:" << ret.size() << endl;
 	for(vector<vector<string>>::iterator it = ret.begin();it!=ret.end();it++)
 	{
 		for(vector<string>::iterator i=it->begin();i!=it->end();i++)
@@ -22,19 +23,20 @@ void Solution_solveNQueens::RunTest()
 
 vector<vector<string>> Solution_solveNQueens::solveNQueens(int n)
 {
-	string line(n, '.');
+	string line(n,'.');
 	solveNQueens(line, 0, n);
 	return ret;
 }
 
 void Solution_solveNQueens::solveNQueens(string& line, int row, int n)
 {
-	if (tmp.size() == n)
+	if (row == n)
 	{
 		ret.push_back(tmp);
 		return;
 	}
-	for(int i =0;i<n;++i)
+
+	for(int i = 0;i<n;i++)
 	{
 		if(checkAll(row, i, n))
 		{
@@ -49,20 +51,14 @@ void Solution_solveNQueens::solveNQueens(string& line, int row, int n)
 
 bool Solution_solveNQueens::checkAll(int row, int col, int n)
 {
-	for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)//左上方
+	for(int i = 0;i<row;i++)
 	{
-		if (tmp[i][j] == 'Q')
-			return false;
-	}
-	for (int i = row - 1; i >= 0; --i)//正上方
-	{
-		if (tmp[i][col] == 'Q')
-			return false;
-	}
-	for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)//右上方
-	{
-		if (tmp[i][j] == 'Q')
-			return false;
+		for(int j =0;j<n;j++)
+		{
+			if (j == col || abs(j - col) == abs(i - row))
+				if (tmp[i][j] == 'Q')
+					return false;
+		}
 	}
 	return true;
 }
