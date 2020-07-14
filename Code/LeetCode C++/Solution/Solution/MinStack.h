edb41b51fb88d7c54416a1fd452d337f983ed961
linge <602633512@qq.com>
2020-07-14 14:25:27 +0800

    https://leetcode-cn.com/problems/min-stack/solution/zui-xiao-zhan-by-leetcode-solution/ 155. 最小栈 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
    \\双栈法，时间复杂度为O(1)

--- Code/LeetCode C++/Solution/Solution/MinStack.h
+++ Code/LeetCode C++/Solution/Solution/MinStack.h
@@ -1,87 +1,32 @@
 #pragma once
-#include "TreeNode.h"
+#include <stack>
+#include <algorithm>
+using namespace std;
 class MinStack {
 public:
 	/** initialize your data structure here. */
-	ListNode* list;
-	vector<int> data;
-	int count = 0;
+	stack<int> data;
+	stack<int> minStack;
 
 	MinStack() {
-		count = 0;
+		minStack.push(INT_MAX);
 	}
 
 	void push(int x) {
-		if (count > 0)
-		{
-			ListNode* dummy = list;
-			ListNode* pre = NULL;
-			ListNode* xNode = new ListNode(x);
-			bool isPosFind = false;
-			while(dummy)
-			{
-				if(dummy->val>x)
-				{
-					if(pre)
-					{
-						pre->next = xNode;
-						xNode->next = dummy;
-					}
-					else
-					{
-						xNode->next = dummy;
-						list = xNode;
-					}
-					isPosFind = true;
-					break;
-				}
-				pre = dummy;
-				dummy = dummy->next;
-			}
-			if(!isPosFind)
-			{
-				pre->next = xNode;
-			}
-		}
-		else
-		{
-			list = new ListNode(x);
-		}
-		data.push_back(x);
-		count++;
+		data.push(x);
+		minStack.push(min(minStack.top(), x));
 	}
 
 	void pop() {
-		int x = data[count-1];
-		count--;
-		data.pop_back();
-		ListNode* dummy = list, *pre = NULL;
-		while(dummy)
-		{
-			if(dummy->val == x)
-			{
-				if(pre)
-				{
-					pre->next = dummy->next;
-					delete dummy;
-				}else
-				{
-					list = dummy->next;
-					delete dummy;
-				}
-				dummy = NULL;
-				break;
-			}
-			pre = dummy;
-			dummy = dummy->next;
-		}
+		data.pop();
+		minStack.pop();
 	}
 
 	int top() {
-		return data[count - 1];
+		return data.top();
 	}
 
 	int getMin() {
-		return list->val;
+		return minStack.top();
 	}
 };
