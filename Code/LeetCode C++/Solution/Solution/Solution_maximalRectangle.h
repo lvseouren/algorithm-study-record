3941deb1dde689ed23685d92d044bbff7149a89f
linge <602633512@qq.com>
2020-07-16 18:10:51 +0800

    \\目前没啥头绪的一道题

---
+++ Code/LeetCode C++/Solution/Solution/Solution_maximalRectangle.h
@@ -0,0 +1,23 @@
+//https://leetcode-cn.com/problems/maximal-rectangle/
+//85. 最大矩形
+//给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+//
+//示例 :
+//
+//输入:
+//[
+//	["1", "0", "1", "0", "0"],
+//	["1", "0", "1", "1", "1"],
+//	["1", "1", "1", "1", "1"],
+//	["1", "0", "0", "1", "0"]
+//]
+//输出: 6
+#pragma once
+#include <vector>
+using namespace std;
+class Solution_maximalRectangle {
+public:
+	int maximalRectangle(vector<vector<char>>& matrix) {
+
+	}
+};
