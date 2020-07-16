d42a2c12a0e4105a3895e41c40225f01a9b898ee
linge <602633512@qq.com>
2020-07-16 18:09:43 +0800

    \\单调栈的另一个简单一点的问题，不需要记录left（自己就是left），只需要记录right

---
+++ Code/LeetCode C++/Solution/Solution/Solution_dailyTemperatures.h
@@ -0,0 +1,45 @@
+﻿//https://leetcode-cn.com/problems/daily-temperatures/
+//请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+//
+//例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+//
+//提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+#pragma once
+#include <vector>
+#include <stack>
+#include "SolutionBase.h"
+using namespace std;
+
+class Solution_dailyTemperatures:SolutionBase {
+public:
+	vector<int> dailyTemperatures(vector<int>& T) {
+		stack<int> st;
+		int cnt = T.size();
+		vector<int> nextIndexTable(cnt, -1);
+		for(int i = 0;i<cnt;i++)
+		{
+			while(!st.empty()&&T[i]>T[st.top()])
+			{
+				nextIndexTable[st.top()] = i;
+				st.pop();
+			}
+			st.push(i);
+		}
+		for(int i = 0;i<cnt;i++)
+		{
+			nextIndexTable[i] = nextIndexTable[i]>0? nextIndexTable[i] - i:0;
+		}
+		return nextIndexTable;
+	}
+
+	void RunTest()
+	{
+		RunTestCase({ 73, 74, 75, 71, 69, 72, 76, 73 });
+	}
+
+	void RunTestCase(vector<int> input)
+	{
+		vector<int> ret = dailyTemperatures(input);
+		vecPrinter->print(ret);
+	}
+};
