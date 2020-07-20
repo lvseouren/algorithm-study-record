d4a36ba750ce1fbef5a6ee2de1eadf31dbe1b67f
linge <602633512@qq.com>
2020-07-20 17:18:41 +0800

    https://leetcode-cn.com/problems/maximum-number-of-non-overlapping-substrings/ 1520. 最多的不重叠子字符串 给你一个只包含小写字母的字符串 s ，你需要找到 s 中最多数目的非空子字符串，满足如下条件：
    这些字符串之间互不重叠，也就是说对于任意两个子字符串 s[i..j] 和 s[k..l] ，要么 j < k 要么 i > l 。
    如果一个子字符串包含字符 c ，那么 s 中所有 c 字符都应该在这个子字符串中。
    请你找到满足上述条件的最多子字符串数目。如果有多个解法有相同的子字符串数目，请返回这些子字符串总长度最小的一个解。可以证明最小总长度解是唯一的。
    请注意，你可以以 任意 顺序返回最优解的子字符串。
    
    \\初版，但是这个算法不能通过所有的测试用例
    \\识别不了这种情况：["ababa"]

--- Code/LeetCode C++/Solution/Solution/Contest_maxNumOfSubstrings.h
+++ Code/LeetCode C++/Solution/Solution/Contest_maxNumOfSubstrings.h
@@ -9,19 +9,50 @@ public:
 	vector<string> ret;
 	vector<string> maxNumOfSubstrings(string s) 
 	{
+		vector<string> ret;
 		if (s.empty())
 			return ret;
 		map<char, int> left;
 		map<char, int> right;
-		for(int i =0;i<s.length();i++)
+		for (int i = 0; i < s.length(); i++)
 		{
-			if (left.find(s[i])==left.end())
+			if (left.find(s[i]) == left.end())
 				left[s[i]] = i;
 			right[s[i]] = i;
 		}
+		for (char c = 'a'; c <= 'z'; c++)
+		{
+			if (left.find(c) != left.end())
+			{
+				if (left[c] == right[c])
+				{
+					ret.push_back(string(1, c));
+				}
+				else
+				{
+					bool skip = false;
+					for (int i = left[c] + 1; i < right[c]; i++)
+					{
+						if (left[s[i]] < left[c])
+						{
+							skip = true;
+							break;
+						}
 
-		recursiveFunc(s, left, right, 0, s.length());
-
+						if (right[s[i]] < right[c])
+						{
+							skip = true;
+							break;
+						}
+						right[c] = right[s[i]];
+					}
+					if (!skip)
+						ret.push_back(s.substr(left[c], right[c] - left[c] + 1));
+				}
+			}
+		}
+		if (ret.empty())
+			ret.push_back(s);
 		return ret;
 	}
 
