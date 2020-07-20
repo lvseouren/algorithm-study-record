f4b7bda4335c53ccbf14fbe4326d48cb69c886d1
linge <602633512@qq.com>
2020-07-20 17:16:07 +0800

    helper template

--- Code/LeetCode C++/Solution/Solution/VectorPrinter.h
+++ Code/LeetCode C++/Solution/Solution/VectorPrinter.h
@@ -6,8 +6,25 @@ using namespace std;
 class VectorPrinter
 {
 public:
-	VectorPrinter();
-	void print(vector<int>);
-	void print(vector<vector<int>>);
+	VectorPrinter(){}
+
+	template<typename T>
+	void print(vector<T>& vec)
+	{
+		for (auto it : vec)
+			cout << it << ",";
+		cout << endl;
+	}
+
+	void print(vector<vector<int>> vec)
+	{
+		for (vector<vector<int>>::iterator it = vec.begin(); it != vec.end(); ++it)
+		{
+			for (vector<int>::iterator i = it->begin(); i != it->end(); ++i)
+				cout << *i << ",";
+			cout << endl;
+		}
+	}
+
 };
 
