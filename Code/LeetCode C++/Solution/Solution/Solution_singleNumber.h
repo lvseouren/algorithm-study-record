71938a09956bc391eba16136c5209fbd1bbf420e
lvseouren <lvseouren@163.com>
2020-07-22 22:26:02 +0800

    https://leetcode-cn.com/problems/single-number-ii/ 137. 只出现一次的数字 II \\很巧妙的解法，但我还是不是很理解逻辑表达式是怎么求出来的

--- Code/LeetCode C++/Solution/Solution/Solution_singleNumber.h
+++ Code/LeetCode C++/Solution/Solution/Solution_singleNumber.h
@@ -7,19 +7,33 @@ public:
     int singleNumber(vector<int>& nums) 
     {
         int ret = 0;
-        for (auto num : nums)
+        for (int num : nums)
             ret ^= num;
         return ret;
     }
 
+    int singleNumber_31(vector<int>& nums)
+    {
+        int X = 0, Y = 0;
+        for(int num:nums)
+        {
+            Y = ~X & (Y ^ num);
+            X = ~Y & (X ^ num);
+        }
+        return Y;
+    }
+
     void RunTest()
     {
-        RunTestCase({ 2,2,1 });
-        RunTestCase({ 4,1,2,1,2 });
+        //RunTestCase({ 2,2,1 });
+        //RunTestCase({ 4,1,2,1,2 });
+
+		RunTestCase({ 2,2,2,1 });
+		RunTestCase({ 4,1,2,1,2,1,2 });
     }
 
     void RunTestCase(vector<int> nums)
     {
-        cout << singleNumber(nums) << endl;
+        cout << singleNumber_31(nums) << endl;
     }
 };
