d311f2bcb23d0b4686708c3bea69c6d2ba810185
linge <602633512@qq.com>
2020-07-22 11:08:13 +0800

    \\BFS解法 \\诀窍是使用队列 \\跟数的BFS的区别在于：树是单源BFS，这个是多源BFS（其实也没啥不同）

--- Code/LeetCode C++/Solution/Solution/Solution_updateMatrix.h
+++ Code/LeetCode C++/Solution/Solution/Solution_updateMatrix.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include "SolutionBase.h"
+#include <queue>
 using namespace std;
 
 class Solution_updateMatrix:SolutionBase {
@@ -31,6 +32,39 @@ public:
 		return ret;
 	}
 
+	vector<vector<int>> updateMatrix_bfs(vector<vector<int>>& matrix)
+	{
+		rowCnt = matrix.size();
+		colCnt = matrix[0].size();
+
+		queue<pair<int, int>> queues;
+		for (int i = 0; i < rowCnt; i++)
+			for (int j = 0; j < colCnt; j++)
+				if (matrix[i][j] == 0)
+					queues.push(pair<int, int>(i, j));
+				else
+					matrix[i][j] = -1;
+
+		int dx[] = { -1, 1, 0, 0 };
+		int dy[] = { 0,0,1,-1 };
+		while(!queues.empty())
+		{
+			pair<int, int> point = queues.front();
+			queues.pop();
+			int x = point.first, y = point.second;
+			for(int i = 0;i<4;i++)
+			{
+				int newX = x + dx[i], newY = y + dy[i];
+				if(newX>=0&&newX<rowCnt&&newY>=0&&newY<colCnt&&matrix[newX][newY]==-1)
+				{
+					matrix[newX][newY] = matrix[x][y] + 1;
+					queues.push(pair<int, int>(newX, newY));
+				}
+			}
+		}
+		return matrix;
+	}
+
 	bool CheckSurroundNum(vector<vector<int>>& matrix, int row, int col, int distance)
 	{
 		int rowStart = row - distance > 0 ? row - distance : 0;
@@ -42,7 +76,7 @@ public:
 		{
 			for(int j = colStart;j <= colEnd;j++)
 			{
-				if(i==row&&j==col||abs(i-row)+abs(j-col)>distance)
+				if(i==row&&j==col||abs(i-row)+abs(j-col)!=distance)
 					continue;
 				if (matrix[i][j] == 0)
 					return true;
@@ -61,7 +95,7 @@ public:
 	{
 		vecPrinter->print(matrix);
 		cout << endl;
-		vector<vector<int>> output = updateMatrix(matrix);
+		vector<vector<int>> output = updateMatrix_bfs(matrix);
 		vecPrinter->print(output);
 		cout << endl;
 	}
