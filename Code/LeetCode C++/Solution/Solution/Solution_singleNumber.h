16fea885360daf92d4e9f228ec8d1db47a639953
linge <602633512@qq.com>
2020-07-23 15:53:01 +0800

    https://leetcode-cn.com/problems/single-number-iii/ 给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。
    \\想了半天还是没想到怎么由a^b求出a和b
    \\看了答案才知道，原来可以通过a和b不同的位来作为过滤器，然后再遍历一遍就能得到a(或者b，等价）,然后b = a^b^a

--- Code/LeetCode C++/Solution/Solution/Solution_singleNumber.h
+++ Code/LeetCode C++/Solution/Solution/Solution_singleNumber.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include "SolutionBase.h"
 using namespace std;
-class Solution_singleNumer {
+class Solution_singleNumer:SolutionBase {
 public:
+    //给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了2次。找出那个只出现了一次的元素。
     int singleNumber(vector<int>& nums) 
     {
         int ret = 0;
@@ -12,6 +14,7 @@ public:
         return ret;
     }
 
+    //给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。
     int singleNumber_31(vector<int>& nums)
     {
         int X = 0, Y = 0;
@@ -23,17 +26,35 @@ public:
         return Y;
     }
 
+    //给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。
+    vector<int> singleNumber_212(vector<int>& nums)
+    {
+        int bitMask = 0;
+        for (int num : nums)
+            bitMask ^= num;
+        int x = 0;
+        int mask = bitMask&-bitMask;
+        for (int num : nums)
+        {
+            if (num & mask)
+                x ^= num;
+        }
+        return { x,x ^ bitMask };
+	}
+
     void RunTest()
     {
         //RunTestCase({ 2,2,1 });
         //RunTestCase({ 4,1,2,1,2 });
 
-		RunTestCase({ 2,2,2,1 });
-		RunTestCase({ 4,1,2,1,2,1,2 });
+	/*	RunTestCase({ 2,2,2,1 });
+		RunTestCase({ 4,1,2,1,2,1,2 });*/
+        RunTestCase({ 1,2,1,3,2,5 });
     }
 
     void RunTestCase(vector<int> nums)
     {
-        cout << singleNumber_31(nums) << endl;
+        vector<int> output = singleNumber_212(nums);
+        vecPrinter->print(output);
     }
 };
