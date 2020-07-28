2e454fc1867b41664dcbea5e6a34a48dcb11589d
linge <602633512@qq.com>
2020-07-28 11:44:40 +0800

    \\时间复杂度O(log(m+n))的算法

--- Code/LeetCode C++/Solution/Solution/Solution_findMedianSortedArrays.h
+++ Code/LeetCode C++/Solution/Solution/Solution_findMedianSortedArrays.h
@@ -6,34 +6,38 @@ class Solution_findMedianSortedArrays {
 public:
 	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
 	{
-		vector<int> ret;
-		int i1 = 0, i2 = 0;
-		int cnt1 = nums1.size(), cnt2 = nums2.size();
-		while(i1< cnt1 &&i2<cnt2)
-		{
-			if (nums1[i1] < nums2[i2])
-				ret.push_back(nums1[i1++]);
-			else
-				ret.push_back(nums2[i2++]);
-		}
-		while(i1<cnt1)
-		{
-			ret.push_back(nums1[i1++]);
-		}
-		while(i2<cnt2)
-		{
-			ret.push_back(nums2[i2++]);
-		}
-		
-		double mid1 = (double)cnt1 / 2;
-		double mid2 = (double)cnt2 / 2;
-		int mid = mid1 + mid2;
-		if(cnt1%2+cnt2%2==1)
-		{
-			return ret[mid];
-		}
+		int totalLength = nums1.size() + nums2.size();
+		if (totalLength % 2 == 1)
+			return getKthElement(nums1, nums2, (totalLength+1) / 2);
 		else
-			return (double)ret[mid] / 2 + (double)ret[mid - 1] / 2;
+			return (getKthElement(nums1, nums2, totalLength / 2) + getKthElement(nums1, nums2, totalLength / 2 + 1))/2.0;
+	}
+
+	int getKthElement(const vector<int>& nums1, const vector<int>& nums2, int k)
+	{
+		int m = nums1.size(), n = nums2.size();
+		int index1 = 0, index2 = 0;
+
+		while(true)
+		{
+			if (index1 == m)
+				return nums2[index2 + k - 1];
+			if (index2 == n)
+				return nums1[index1 + k - 1];
+			if (k == 1)
+				return min(nums1[index1], nums2[index2]);
+
+			int newIndex1 = min(index1 + k / 2 - 1, m - 1), newIndex2 = min(index2 + k / 2 - 1, n - 1);
+			if(nums1[newIndex1]<=nums2[newIndex2])
+			{
+				k -= newIndex1-index1+1;
+				index1 = newIndex1 + 1;
+			}else
+			{
+				k -= newIndex2-index2+1;
+				index2 = newIndex2 + 1;
+			}
+		}
 	}
 
 	void RunTest()
