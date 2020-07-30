5cff97cdd35d60d5af85baa968cdb82d444f9516
linge <602633512@qq.com>
2020-07-30 10:18:20 +0800

    \\莫名其妙AC了 \\回想一下，其实只要数组有至少1个元素，那么就肯定存在最大值，因为nums[-1],nums[n]都为INT_MIN

--- Code/LeetCode C++/Solution/Solution/Solution_findPeakElement.h
+++ Code/LeetCode C++/Solution/Solution/Solution_findPeakElement.h
@@ -6,13 +6,59 @@ class Solution_findPeakElement:SolutionBase {
 public:
 	int findPeakElement(vector<int>& nums) 
 	{
-
+		int numsCnt = nums.size();
+		if (numsCnt == 0)
+			return -1;
+		else if (numsCnt == 1)
+			return 0;
+		int left = 0, right = numsCnt;
+		while(left<right)
+		{
+			int mid = left + (right - left) / 2;
+			if(mid>0&&mid< numsCnt-1)
+			{
+				if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1])
+					left = mid + 1;
+				else
+					right = mid;
+			}else
+			{
+				if (mid == 0)
+				{
+					if (nums[mid+1] < nums[mid])
+						return mid;
+					else
+						left = mid + 1;
+				}else
+				{
+					if (nums[mid] > nums[mid - 1])
+						return mid;
+					else
+						right = mid;
+				}
+			}
+		}
+		if (left >= 0 && left < numsCnt)
+		{
+			int pre = left - 1;
+			int next = left + 1;
+			int preValue = pre >= 0 ? nums[pre] : INT_MIN;
+			int nextValue = next < numsCnt ? nums[next] : INT_MIN;
+			if((preValue<nums[left]&&nextValue<nums[left])||(preValue>nums[left]&&nextValue>nums[left]))
+				return left;
+		}
+		return -1;
 	}
 
 	void RunTest()
 	{
 		RunTestCase({1,2,3,1});
 		RunTestCase({ 1,2,1,3,5,6,4 });
+		RunTestCase({ 1,2,1,3,5,6,7 });
+		RunTestCase({ 3,2,6,6,5});
+		RunTestCase({ 1 });
+		RunTestCase({ 1,1 });
+		RunTestCase({ 2,1,2 });
 	}
 
 	void RunTestCase(vector<int> nums)
