61a1801d27a6710ba7a1144c43a2fca5fd4b28f8
linge <602633512@qq.com>
2020-08-25 15:34:18 +0800

    \\https://leetcode-cn.com/problems/triangle/ \\三角形最小路径和 \\错了两次，第三次终于AC了，全程没看答案，提交后看题解，发现跟我的一样。 \\前面错的两次，都是因为没有找到正确的dp[i] = f(dp[-1])，导致求出的答案都是局部最优，最后悟了，要想获得全局最优，还是得遍历一下所有路径，并且每一个节点的最小路径和应该是在上一层节点的最小路径和基础上求出

--- Code/LeetCode C++/Solution/Solution/Solution_minimumTotal.h
+++ Code/LeetCode C++/Solution/Solution/Solution_minimumTotal.h
@@ -7,30 +7,41 @@ class Solution_minimumtotal
 public:
 	int minimumTotal(vector<vector<int>>& triangle) 
 	{
-		vector<int> dp;
-		for (int i = 0; i < triangle.size(); i++)
+		int raw = triangle.size();
+		vector<vector<int>> dp(raw,vector<int>(raw,0));
+		
+		int curMin = INT_MAX;
+		for (int i = 0; i < raw; ++i)
 		{
-			if (i > 0)
-				dp[i] = dp[i - 1] + minRawNum(triangle[i]);
-			else
-				dp[i] = minRawNum(triangle[i]);
+			int colCnt = triangle[i].size();
+			for (int j = 0; j < colCnt; ++j)
+			{
+				if (i == 0)
+					dp[i][j] = triangle[i][j];
+				else
+				{
+					dp[i][j] = min(j>0?dp[i - 1][j - 1]:INT_MAX, j<colCnt-1?dp[i - 1][j]:INT_MAX) + triangle[i][j];
+				}
+				if (i == raw - 1)
+					curMin = min(dp[i][j], curMin);
+			}
 		}
-	}
-
-	int minRawNum(vector<int>& nums)
-	{
-		return 1;
+		return curMin;
 	}
 
 	void RunTest()
 	{
 		RunTestCase({ {2}, {3,4},{6,5,7 },{4,1,8,3} });
+		RunTestCase({ {1}, {1,1},{1,2,1 },{1000,1000,2,2} });
+		RunTestCase({ {1}, {1,1},{1,2,2 },{1,2,2,1},{1000,1000,2,2,2} });
+		RunTestCase({{1}, {-2, -5}, {3, 6, 9}, {-1, 2, 4, -3}} );
+		RunTestCase({ {-1}, {9, -2}, {0, 4, 5}, {7, 4, -4, -5}, {9, 6, 0, 5, 7}, {9, 2, -9, -4, 5, -2}, {-4, -9, -5, -7, -5, -5, -2}, {-9, 5, -6, -4, 4, 1, 6, -4}, {-4, 3, 9, -2, 8, 6, -9, -2, -2}, {7, -6, 9, 8, -4, 2, -4, -2, -1, -2}, {0, 3, 2, 4, 0, -6, 7, 6, 7, -5, 2}, {9, 0, -8, 6, 4, 6, 2, 5, -9, 9, -1, -6}, {6, -3, -4, -5, 0, 3, 3, 4, -6, -4, -7, 7, 3} });
 	}
 
 	void RunTestCase(vector<vector<int>> triangle)
 	{
 		int output = minimumTotal(triangle);
-		cout << "输出为：" << output << endl;
+		cout << "输出为：" << output << endl<<endl;
 	}
 };
 
