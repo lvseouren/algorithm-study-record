230225b0f7986fd24ee92a004f925eb030c32144
linge <602633512@qq.com>
2020-08-26 16:36:52 +0800

    https://leetcode-cn.com/problems/palindrome-partitioning-ii/ \\分割回文串 \\好难，公式都不知道咋套的感觉

---
+++ Code/LeetCode C++/Solution/Solution/Solution_minCut.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "SolutionBase.h"
+class Solution_minCut:SolutionBase {
+public:
+    int minCut(string s) 
+    {
+
+    }
+
+    void RunTest()
+    {
+
+    }
+
+    void RunTestCase(string s)
+    {
+        cout << "Input:" << s << endl;
+        cout << "Output:" << minCut(s) << endl << endl;
+    }
+};
