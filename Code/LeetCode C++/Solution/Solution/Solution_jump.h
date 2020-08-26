42b1ca679e9014eee55018b3b70088a9d519d2b3
linge <602633512@qq.com>
2020-08-26 10:16:43 +0800

    \\还是不行，当nums里“1”很多的时候，又变成O(N^2)了

--- Code/LeetCode C++/Solution/Solution/Solution_jump.h
+++ Code/LeetCode C++/Solution/Solution/Solution_jump.h
@@ -7,23 +7,32 @@ public:
 		int cnt = nums.size();
 		vector<int> dp(cnt, INT_MAX);
 		dp[0] = 0;
+		map<int, int> mapToLastIndex;
+		mapToLastIndex[0] = 0;
 		for (int i = 0; i < cnt; ++i)
 		{
-			for (int j = 1; j <= nums[i]; ++j)
+			dp[i] = getDp(i, mapToLastIndex);
+			mapToLastIndex[dp[i] + 1] = mapToLastIndex.find(dp[i]+1)==mapToLastIndex.end()? i + nums[i]:max(mapToLastIndex[dp[i]+1],i+nums[i]);
+		}
+		return getDp(cnt-1,mapToLastIndex);
+	}
+
+	int getDp(int index, map<int,int>& data)
+	{
+		int ret = INT_MAX;
+		for (auto value : data)
+		{
+			if (value.second >= index&&ret>value.first)
 			{
-				if (i + j >= cnt)
-					break;
-				dp[i + j] = min(dp[i] + 1, dp[i+j]);
+				ret = value.first;
 			}
 		}
-		return dp[cnt - 1];
+		return ret;
 	}
 
 	void RunTest()
 	{
 		RunTestCase({ 2,3,1,1,4 });
-		RunTestCase({ 3,2,1,0,4 });
-		RunTestCase({ 0,2,3 });
 		RunTestCase({ 0 });
 	}
 
