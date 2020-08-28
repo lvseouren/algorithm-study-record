d27e7ac7b5177da11f7d65be19fb17489c34d960
linge <602633512@qq.com>
2020-08-28 10:28:38 +0800

    \\抄的答案 \\总算理解了 \\时间复杂度应该是O(N^2) \\感悟：不要一开始就在那钻牛角尖想牛逼的算法，先脚踏实地将问题解决了，再去想优化。 \\这道题让我想起了第一道DP：三角形最小路径——就是需要双层for循环，否则得不到全局最优解

--- Code/LeetCode C++/Solution/Solution/Solution_minCut.h
+++ Code/LeetCode C++/Solution/Solution/Solution_minCut.h
@@ -1,15 +1,48 @@
 #pragma once
 #include "SolutionBase.h"
-class Solution_minCut:SolutionBase {
+class Solution_minCut :SolutionBase {
 public:
-    int minCut(string s) 
-    {
+	int minCut(string s) 
+	{
+        int len = s.size();
+        vector<int> dp(len, INT_MAX);
+        vector<vector<bool>> isPalindrome(len, vector<bool>(len, false));
 
-    }
+        for(int right = 0;right<len;right++)
+            for (int left = 0; left <= right; left++)
+            {
+                isPalindrome[left][right] = (s[left] == s[right]) && (right - left <= 1 || isPalindrome[left + 1][right - 1]);
+            }
+
+        for (int i = 0; i < len; i++)
+        {
+            if (isPalindrome[0][i])
+            {
+                dp[i] = 0;
+                continue;
+            }
+
+            for (int j = 0; j < i; j++)
+            {
+                if (isPalindrome[j + 1][i])
+                    dp[i] = min(dp[i], 1 + dp[j]);
+            }
+        }
+        return dp[len - 1];
+	}
+	
+
 
     void RunTest()
     {
-
+        RunTestCase("a");
+        RunTestCase("ab");
+        RunTestCase("aa");
+        RunTestCase("aab");
+        RunTestCase("aabaa");
+        RunTestCase("abcba");
+        RunTestCase("abcbd");
+        RunTestCase("abcdb");
     }
 
     void RunTestCase(string s)
