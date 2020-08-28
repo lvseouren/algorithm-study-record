b9929115a1968f4842b52a3dd65b868aad3a2762
linge <602633512@qq.com>
2020-08-28 12:02:14 +0800

    \\马拉车会写了 \\其实根本没那么复杂 \\主要参考了这个： https://www.cxyxiaowu.com/2665.html#:~:text=%E9%A9%AC%E6%8B%89%E8%BD%A6%E7%AE%97%E6%B3%95%EF%BC%88%20Manacher,%E5%A4%8D%E6%9D%82%E5%BA%A6%E6%8F%90%E5%8D%87%E5%88%B0%E4%BA%86%20%E7%BA%BF%E6%80%A7%20%E3%80%82

--- Code/LeetCode C++/Solution/Solution/Solution_minCut.h
+++ Code/LeetCode C++/Solution/Solution/Solution_minCut.h
@@ -31,10 +31,65 @@ public:
         return dp[len - 1];
 	}
 	
+    string longestPalindrome(string inputS)
+    {
+        int pos = 0;
+        int maxLen = 0;
 
+        string s = addBoundaries(inputS,'#');
+        int cnt = s.size();
+        int R=0,center=0;
+        vector<int> dp(cnt, 0);
+        for (int i = 0; i < cnt; i++)
+        {
+            if (i < R)
+            {  
+                int mirror = 2 * center - i;
+            /*    if (dp[mirror] <= R - i)
+                    dp[i] = dp[mirror];
+                else
+                    dp[i] = R - i;*/
+                dp[i] = min(dp[mirror], R - i);
+            }
+			int left = i - (1+dp[i]);
+			int right = i + (1+dp[i]);
+			while (left >= 0 && right < cnt && s[left] == s[right])
+			{
+				left--;
+				right++;
+				dp[i]++;
+			}
+			if (dp[i] > maxLen)
+			{
+                maxLen = dp[i];
+                pos = (i - maxLen)/2;
+			}
+			if (i+dp[i] > R)
+			{
+				R = i+dp[i];
+				center = i;
+			}
+        }
+
+        return inputS.substr(pos, maxLen);
+    }
+
+	string addBoundaries(string s, char divide)
+	{
+        int cnt = s.size();
+        if (cnt == 0)
+            return s;
+        string ret = string(cnt * 2 + 1, divide);
+        for (int i = 0; i < cnt; ++i)
+        {
+            ret[i << 1 | 1] = s[i];
+        }
+        return ret;
+	}
 
     void RunTest()
-    {
+	{
+		RunTestCase("");
         RunTestCase("a");
         RunTestCase("ab");
         RunTestCase("aa");
@@ -43,11 +98,13 @@ public:
         RunTestCase("abcba");
         RunTestCase("abcbd");
         RunTestCase("abcdb");
+        RunTestCase("abccbb");
     }
 
     void RunTestCase(string s)
     {
         cout << "Input:" << s << endl;
-        cout << "Output:" << minCut(s) << endl << endl;
+        //cout << "Output:" << minCut(s) << endl << endl;
+        cout << "Output:" << longestPalindrome(s) << endl << endl;
     }
 };
