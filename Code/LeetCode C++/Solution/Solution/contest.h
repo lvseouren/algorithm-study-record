894c54241c3707f7d3418d54c8982731c3884832
lvseouren <lvseouren@163.com>
2020-09-13 12:11:28 +0800

    sup code

--- Code/LeetCode C++/Solution/Solution/contest.h
+++ Code/LeetCode C++/Solution/Solution/contest.h
@@ -3,15 +3,16 @@
 #include "Contest_countSubTrees.h"
 #include "Contest_204_1.h"
 #include "Contest_204_2.h"
-#include "Contest_204_3.h"
 #include "Contest_204_4.h"
+#include "Contest_206_2.h"
 
 class contest
 {
 public:
 	void RunTest()
 	{
-		RunTest_204();
+		//RunTest_204();
+		RunTest_206();
 	}
 
 	void RunTest_198()
@@ -29,13 +30,17 @@ public:
 		//Contest_204_1 c1;
 		//c1.RunTest();
 
-		Contest_204_2 c2;
-		c2.RunTest();
+		//Contest_204_2 c2;
+		//c2.RunTest();
 
-		//Contest_204_3 c3;
-		//c3.RunTest();
 
 		//Contest_204_4 c4;
 		//c4.RunTest();
+	}
+
+	void RunTest_206()
+	{
+		Contest_206_2 c3;
+		c3.RunTest();
 	}
 };
