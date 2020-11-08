ccc2d33b6c7176a722bf827c85330b325eade4dd
huiseouren <602633512@qq.com>
2020-11-08 18:35:29 +0800

    https://leetcode-cn.com/problems/sell-diminishing-valued-colored-balls/
    \\虽然错了n次，最后还是双100AC了

--- Code/LeetCode C++/Solution/Solution/Solution_maxProfit.h
+++ Code/LeetCode C++/Solution/Solution/Solution_maxProfit.h
@@ -7,29 +7,65 @@ public:
 	int maxProfit(vector<int>& inventory, int orders) 
 	{
 		ll sum = 0;
-		int cnt = inventory.size();
-		for (int i = 0; i < cnt; ++i)
-			sum += inventory[i];
-		ll rest = sum - orders;
-		ll average = ceil(rest*1.0 / cnt);
+		vector<int> realInventory;
+		ll average = INT_MAX;
+		vector<int>* frontBuffer = &inventory;
+		vector<int>* backBuffer = &realInventory;
+		vector<int>* finalData;
+		while(true)
+		{
+			finalData = frontBuffer;
+			backBuffer->clear();
+
+			sum = 0;
+			int cnt = frontBuffer->size();
+			for (int i = 0; i < cnt; ++i)
+			{
+				sum += (*frontBuffer)[i];
+			}
+			ll rest = sum - orders;
+			average = ceil(rest * 1.0 / cnt);
+			
+			bool canBreak = true;
+			for (int i = 0; i < cnt; ++i)
+			{
+				if ((*frontBuffer)[i] < average)
+				{
+					canBreak = false;
+					break;
+				}
+			}
+			if (canBreak)
+				break;
+
+			for (int i = 0; i < cnt; ++i)
+			{
+				if ((*frontBuffer)[i] >= average)
+				{
+					backBuffer->push_back((*frontBuffer)[i]);
+				}
+			}
+			auto tmp = frontBuffer;
+			frontBuffer = backBuffer;
+			backBuffer = tmp;
+		}
+
+		int cnt = finalData->size();
 		ll d = sum - average * cnt;
 		ll differ = orders - d;
 		ll ret = 0;
 		int modular = 1e9 + 7;
 		for(int i = 0;i<cnt;++i)
 		{
-			long long curPrice = inventory[i];
-			//for (int j = 0; j < inventory[i] - average; ++j)
-			//{
-			//	ret += curPrice;
-			//	curPrice--;
-			//	ret %= modular;
-			//}
+			long long curPrice = (*finalData)[i];
 
 			long long tmp1 = (curPrice + average + 1) ;
 			long long tmp2 = (curPrice - average) ;
+			if (tmp1 % 2 == 0)
+				tmp1 /= 2;
+			else
+				tmp2 /= 2;
 			long long price = mod_mult(tmp1, tmp2, modular);
-			price /= 2;
 			price %= modular;
 			ret += price;
 			ret %= modular;
@@ -58,11 +94,13 @@ public:
 
 	void RunTest()
 	{
-		//RunTestCase({ 2,5 }, 4);
-		//RunTestCase({ 3,5 }, 6);
-		//RunTestCase({ 2,8,4,10,6 }, 20);
-		//RunTestCase({ 1000000000 }, 1000000000);
-		//RunTestCase({ 773160767}, 252264991);
+		RunTestCase({701695700, 915736894, 35093938, 364836059, 452183894, 951826038, 861556610, 441929847, 842650446, 858413011, 457896886, 35119509, 776620034, 396643588, 83585103, 681609037},598226067);
+		RunTestCase({ 680754224, 895956841, 524658639, 3161416, 992716630, 7365440, 582714485, 422256708, 332815744, 269407767 }, 707568720);
+		RunTestCase({ 2,5 }, 4);
+		RunTestCase({ 3,5 }, 6);
+		RunTestCase({ 2,8,4,10,6 }, 20);
+		RunTestCase({ 1000000000 }, 1000000000);
+		RunTestCase({ 773160767}, 252264991);
 		RunTestCase({ 497978859,167261111,483575207,591815159 }, 836556809);
 	}
 
