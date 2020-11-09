926aade6e7cbfdf82dbf488df6291210415bc1a5
lvseouren <602633512@qq.com>
2020-11-09 16:07:38 +0800

    https://leetcode-cn.com/problems/smallest-range-covering-elements-from-k-lists/
    \\这题挂太久了，抄个答案
    \\但其实答案都没抄明白
    \\不过这个题解讲得挺清楚的，没细看，貌似是同一个思路：https://leetcode-cn.com/problems/smallest-range-covering-elements-from-k-lists/solution/tan-xin-duo-zhi-zhen-you-xian-ji-dui-lie-by-girapa/

--- Code/LeetCode C++/Solution/Solution/Solution_smallestRange.h
+++ Code/LeetCode C++/Solution/Solution/Solution_smallestRange.h
@@ -9,17 +9,52 @@ class Solution_smallestRange :SolutionBase {
 public:
 	vector<int> smallestRange(vector<vector<int>>& nums) 
 	{
+		int rangeLeft = 0, rangeRight = INT_MAX;
+		int size = nums.size();
+		vector<int> next(size);
 
+		auto cmp = [&](const int& u, const int& v) {
+			return nums[u][next[u]] > nums[v][next[v]];
+		};
+		priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+		int minValue = 0, maxValue = INT_MIN;
+		for (int i = 0; i < size; ++i) {
+			pq.emplace(i);
+			maxValue = max(maxValue, nums[i][0]);
+		}
+
+		while (true) {
+			int row = pq.top();
+			pq.pop();
+			minValue = nums[row][next[row]];
+			if (maxValue - minValue < rangeRight - rangeLeft) {
+				rangeLeft = minValue;
+				rangeRight = maxValue;
+			}
+			if (next[row] == nums[row].size() - 1) {
+				break;
+			}
+			++next[row];
+			maxValue = max(maxValue, nums[row][next[row]]);
+			pq.emplace(row);
+		}
+
+		return { rangeLeft, rangeRight };
 	}
 
 	void RunTest()
 	{
-		RunTestCase();
+		RunTestCase({{4, 10, 15, 24, 26}, {0, 9, 12, 20}, {5, 18, 22, 30}} );
+		RunTestCase({{1, 2, 3}, {1, 2, 3}, {1, 2, 3}} );
+		RunTestCase({{10, 10}, {11, 11}} );
+		RunTestCase({{10}, {11}} );
+		RunTestCase({{1}, {2}, {3}, {4}, {5}, {6}, {7}} );
 	}
 
-	void RunTestCase()
+	void RunTestCase(vector<vector<int>> nums)
 	{
-		cout << "Input:" << endl;
-		//cout << "Output:"<< smallestRange() << endl << endl;
+		cout << "Output:"<< endl;
+		vector<int> output = smallestRange(nums);
+		vecPrinter->print(output);
 	}
 };
