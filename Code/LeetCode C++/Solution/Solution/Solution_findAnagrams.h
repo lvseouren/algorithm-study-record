f7ecee9d42723c79e84551fc2467655abbe4e739
lvseouren <602633512@qq.com>
2020-11-09 20:14:21 +0800

    438. 找到字符串中所有字母异位词
    \\优化：160ms到8ms

--- Code/LeetCode C++/Solution/Solution/Solution_findAnagrams.h
+++ Code/LeetCode C++/Solution/Solution/Solution_findAnagrams.h
@@ -2,79 +2,37 @@
 #include "SolutionBase.h"
 class Solution_findAnagrams :SolutionBase {
 public:
-	vector<int> findAnagrams(string s2, string s1) 
+	vector<int> findAnagrams(string s, string p) 
 	{
-		map<char, int> groupData;
-		int groupCnt = 0;
-		for (int i = 0; i < s1.size(); ++i)
+		int windowSize = p.size();
+		int groupData[26] = {};
+
+		for (int i = 0; i < windowSize; ++i)
 		{
-			if (groupData.find(s1[i]) == groupData.end())
-			{
-				groupData[s1[i]] = 1;
-				groupCnt++;
-			}
-			else
-				groupData[s1[i]]++;
+			groupData[p[i] - 'a']++;
 		}
 
-		int left = 0, right = 0, curMatchGrpCnt = 0;
-		map<char, int> cntDict;
+		int curMatchGroupCnt = 0;
 		vector<int> ret;
-		while (right < s2.size())
+		int left = 0, right = 0;
+		while (right < s.size())
 		{
-			char groupId = s2[right];
-			if (groupData.find(groupId) != groupData.end())
-			{
-				if (cntDict.count(groupId) > 0)
-				{
-					cntDict[groupId]++;
-				}
-				else
-				{
-					cntDict[groupId] = 1;
-				}
-				if (cntDict[groupId] == groupData[groupId])
-					curMatchGrpCnt++;
-				if (cntDict[groupId] > groupData[groupId])
-				{
-					while (true)
-					{
-						char leftGroupId = s2[left++];
-						if (leftGroupId == groupId)
-						{
-							cntDict[leftGroupId]--;
-							break;
-						}
-						else
-						{
-							if (cntDict.count(leftGroupId))
-							{
-								cntDict[leftGroupId]--;
-								if (cntDict[leftGroupId] + 1 == groupData[leftGroupId])
-									curMatchGrpCnt--;
-							}
-						}
-					}
+			int rightId = s[right] - 'a';
+			if (groupData[rightId] > 0)
+				curMatchGroupCnt++;
 
-				}
-
-			}
-			else
-			{
-				cntDict.clear();
-				left = right + 1;
-				curMatchGrpCnt = 0;
-			}
-
-			if (curMatchGrpCnt == groupCnt)
-			{
-				ret.push_back(left);
-				char leftGroupId = s2[left++];
-				cntDict[leftGroupId]--;
-				if (cntDict[leftGroupId] + 1 == groupData[leftGroupId])
-					curMatchGrpCnt--;
-			}
+			--groupData[rightId];
 			right++;
+			while (curMatchGroupCnt == windowSize)
+			{
+				if (right - left == windowSize)
+					ret.push_back(left);
+				int leftId = s[left] - 'a';
+				if (groupData[leftId] == 0)
+					curMatchGroupCnt--;
+				groupData[leftId]++;
+				left++;
+			}
 		}
 		return ret;
 	}
